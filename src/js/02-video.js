1a73f43253746134799dcebb49a4697456f29841
Aleksandr Cheverda <alexar@ua.fm>
2022-09-06 21:54:36 +0300

    Завдання 2
    додано вирішення завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeStop, 1000));
+    
+function timeStop(timeupdate) {
+    let pause = timeupdate.seconds;
+    console.log(pause);
+    localStorage.setItem('videoplayer-current-time', pause)
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
