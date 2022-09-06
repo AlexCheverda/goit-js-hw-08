ebb9c160b97393f4a1eddfcd17c7ae6dbb4e4654
Aleksandr Cheverda <alexar@ua.fm>
2022-09-06 18:13:33 +0300

    Завдання 1
    Додано вирішення завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryCard = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
+
+galleryCard.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createCardsMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+        `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.9,
+    captionsData: 'alt',
+    captionDelay: 250,
+    animationSpeed: 250,
+    disableRightClick: true,
+    alertError: false,
+    doubleTapZoom: 2,
+    maxZoom: 5,
+});
 // Change code below this line
 
 console.log(galleryItems);
